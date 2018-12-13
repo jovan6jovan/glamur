e38ba72e9666d70c25fbb9c0b5f0e729d6857aff
Jovan Jovanovic <jovan6jovanovic@gmail.com>
2018-12-13 16:51:19 +0100

    Adedd about us page

--- js/script.js
+++ js/script.js
@@ -24,12 +24,6 @@ function topFunction() {
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
 
 // GALLERY (inspired by Brad Traversy)
 var current = document.querySelector("#current");
